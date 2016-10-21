d939cde14fd9cea0854c5beeea613d1f9eafe6c5
OSerhii <sergiy.olshanskiy@gmail.com>
2016-10-21 19:32:03 +0300

    Create driver for dgf_financial_simple & dgf_other_simple

--- brizol_service.py
+++ brizol_service.py
@@ -1,26 +1,27 @@
- #!/usr/bin/python
- # -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
+from datetime import datetime
 from iso8601 import parse_date
-from robot.api import logger
+import os
 
 
 def subtract_from_time(date_time, subtr_min, subtr_sec):
     sub = datetime.strptime(date_time, "%d/%m/%Y %H:%M:%S")
     return str(sub).replace(' ', 'T') + '.000000+03:00'
 
+
 def convert_datetime_to_brizol_format(isodate):
     iso_dt = parse_date(isodate)
     day_string = iso_dt.strftime("%d/%m/%Y %H:%M")
     return day_string
+
 
 def convert_string_from_dict_brizol(string):
     return {
         u"грн": u"UAH",
         u"True": u"1",
         u"False": u"0",
-        u"Відкриті торги": u"aboveThresholdUA",
         u'Код CAV': u'CAV',
         u'з урахуванням ПДВ': True,
         u'без урахуванням ПДВ': False,
@@ -29,15 +30,18 @@ def convert_string_from_dict_brizol(string):
         u'АУКЦIОН': u'active.auction',
     }.get(string, string)
 
-def adapt_procuringEntity(tender_data):
-    tender_data['data']['procuringEntity']['name'] = u"Ольмек"
+
+def adapt_procuringEntity(role_name, tender_data):
+    if role_name == 'tender_owner':
+        tender_data['data']['procuringEntity']['name'] = u"Ольмек"
     return tender_data
+
 
 def adapt_view_data(value, field_name):
     if field_name == 'value.amount':
         value = float(value.split(' ')[0])
     elif field_name == 'value.currency':
-        value = value.split(' ')[1]    
+        value = value.split(' ')[1]
     elif field_name == 'value.valueAddedTaxIncluded':
         value = ' '.join(value.split(' ')[2:])
     elif field_name == 'minimalStep.amount':
@@ -51,3 +55,15 @@ def adapt_view_data(value, field_name):
     elif 'Date' in field_name:
         value = subtract_from_time(value, 0, 0)
     return convert_string_from_dict_brizol(value)
+
+
+def adapt_view_item_data(value, field_name):
+    if 'unit.name' in field_name:
+        value = ' '.join(value.split(' ')[1:])
+    elif 'quantity' in field_name:
+        value = float(value.split(' ')[0])
+    return convert_string_from_dict_brizol(value)
+
+
+def get_upload_file_path():
+    return os.path.join(os.getcwd(), 'src/robot_tests.broker.brizol/testFileForUpload.txt')
